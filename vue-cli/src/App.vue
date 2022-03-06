694ef43ba4b745b3d007324384b6adda111e628a
uvin1016 <uvin1016@naver.com>
2022-03-06 22:57:31 +0900

    강좌 완료

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewstr" />
+  </div>
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue';
+
+  export default {
+    data: function(){
+      return {
+        str: 'Header'
+      }
+    },
+    components: {
+      AppHeader
+    },
+    methods: {
+      renewstr: function(){
+        this.str = 'hi';
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id:</label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">pw:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(){
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(function(reponse){
+        console.log(reponse);
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
